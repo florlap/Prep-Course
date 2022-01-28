c7398a139f6cbc8f07a9cbd8621184ddfa492d7c
florlap <florencialapadatoff@gmail.com>
2022-01-28 17:02:01 -0300

    prueba de test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,23 +1,25 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola';
+const nuevaString = 'florencia';
+
+
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = (2); 
+
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
+ 
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
-
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
-
+const nuevoModulo = 21 % 5 === 1; 
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
@@ -28,39 +30,47 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str; 
+  } 
   
-}
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
-  
-}
+  // Tu código: 
+   var suma = 6 + 4;
+   return suma;
+} 
+
+
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  var resta = 10 - 2;
+  return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+
 }
+
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  var producto = 10 / 2;
+  return producto;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x = y) 
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -108,13 +118,13 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  Math.pow(4,2);  //16
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  Math.pow(3,3);  //27
 }
 
 function elevar(num, exponent) {
