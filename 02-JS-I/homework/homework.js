af707626e0fb34345fea8b22050f7bd80509c9b5
florlap <florencialapadatoff@gmail.com>
2022-02-04 02:36:58 -0300

    33pasaron

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -250,6 +250,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  // h
   if (letra.length >1){
     return "Dato incorrecto"
   }
